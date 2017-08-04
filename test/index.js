8e90e490a36f44eb8cd0f375eb178becd18d1f26
Daniel Cherubini <daniel@cherubini.casa>
2017-08-04 17:14:32 +0200

    fixes tests

--- test/index.js
+++ test/index.js
@@ -1,8 +1,8 @@
-import test   from 'ava';
+import test from 'ava';
 import config from '../app/config';
 
 test('config', t => {
-    if (config.env === 'dev') {
+    if (config.env === 'development') {
         t.pass()
     } else {
         t.fail()
